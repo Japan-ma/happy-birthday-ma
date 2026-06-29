6d56c7cf0f84ec5a0490fc8096812d0ead43500c
Japan-ma <75016263+Japan-ma@users.noreply.github.com>
2026-06-29 18:12:16 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -195,3 +195,29 @@ if (replayStory) {
     });
 
 }
+// ===============================
+// Floating Hearts
+// ===============================
+
+function createHeart() {
+
+    const heart = document.createElement("div");
+
+    heart.className = "heart";
+
+    heart.innerHTML = "💜";
+
+    heart.style.left = Math.random() * 100 + "%";
+
+    heart.style.animationDuration =
+        (5 + Math.random() * 5) + "s";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 10000);
+
+}
+
+setInterval(createHeart, 800);
